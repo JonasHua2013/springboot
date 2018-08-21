86b91d99f2860280652b6c87853cabdc6c3fdba8
jonashua2013 <jonashua0410@gmail.com>
2018-08-21 13:45:46 +0800

    --解决mysql中文乱码

--- src/main/java/com/practice/demo/controller/HelloController.java
+++ src/main/java/com/practice/demo/controller/HelloController.java
@@ -1,7 +1,12 @@
 package com.practice.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.practice.demo.dao.UserRepository;
+import com.practice.demo.domain.User;
 
 /**
  * 测试controller
@@ -13,8 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@RequestMapping("/hello")
 	public String hello(){
 		return "hello world";
+	}
+	
+	@RequestMapping("/getUser")
+	@Cacheable(value="user-key")
+	public User getUser() {
+	    User user=userRepository.findByUserName("bb");
+	    System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");  
+	    return user;
 	}
 }
